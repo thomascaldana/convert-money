41e224e8739f3e10dd72d698adef205e3fbf49d9
thomascaldana <thomascaldana@gmail.com>
2023-05-31 21:55:11 -0300

    consuming an api with currency value in real-time

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 const button = document.getElementById('convert-button')
 const select = document.getElementById('currency-select')
 
-const dolar = 5.2
-const euro = 5.9
 
-const convertValues = () => {
-    const inputReais = document.getElementById('input-real').value
+const convertValues = async () => {
+    let inputReais = document.getElementById('input-real').value
     const realValueText = document.getElementById('real-value-text')
 
+    // async await
+    const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
+    .then( response => response.json());
 
+    const dolar = data.USDBRL.high
+    const euro = data.EURBRL.high
+    console.log(data)
 
     const currencyValueText = document.getElementById('currency-value-text')
 
